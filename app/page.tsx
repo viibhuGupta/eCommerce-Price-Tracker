526d64d1d11d86490269eb9ea8ccd95745654acc
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-05-17 20:06:39 +0530

    🔥 Navbar dynamic Import images using map function

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,22 @@ import Image from "next/image";
 export default function Home() {
   return (
    <>
+    <section className="px-6 md:mx-20 py-24  border-2 border-red-400">
+      <div className="flex ">
+        <div className="flex justify-center flex-col">
+          <p className="flex gap-2 text-rose-600 text-sm font-medium">
+            Smart Shoping Starts Here
 
-    <h1>Hello</h1>
+            <Image 
+            src="/assets/icons/arrow-right.svg"
+            width={16}
+            height={16}
+            alt="arrow icon"
+            />
+          </p>
+        </div>
+      </div>
+    </section>
    </>
   );
 }
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,13 +2,27 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
-
+const navIcons = [
+    {
+        src:'/assets/icons/search.svg'  ,
+        alt : 'search'
+    },
+    {
+        src:'/assets/icons/black-heart.svg'  ,
+        alt : 'heart'
+    },
+    {
+        src:'/assets/icons/user.svg'  ,
+        alt : 'user'
+    },
+    
+]
 
 const Navbar = () => {
   return (
     <>
     <header className='w-full '>
-        <nav className='flex justify-between item-center px-6 bg-slate-400 md:px-20 py-4'>
+        <nav className='flex justify-between item-center px-6 md:px-20 py-4'>
             <Link className='flex' href="#/">
                 <Image 
                 src="/assets/icons/logo.svg"
@@ -17,8 +31,21 @@ const Navbar = () => {
                 alt='logo'
                 
                 />
-                <p className='font-spaceGrotesk text-[21px] font-bold'>Price<span className='text-rose-700  '>wise</span></p>
+                <p className='font-spaceGrotesk text-[21px] font-bold'>Price<span className='text-rose-700'>wise</span> </p>
             </Link>
+
+            <div className="flex items-center gap-4">
+                {navIcons.map((icon) => (
+                    <Image
+                    key={icon.alt}
+                    src={icon.src}
+                    alt={icon.alt}
+                    width={25}
+                    height={25}
+                    className=' object-contain cursor-pointer'
+                    />
+                ))}
+            </div>
         </nav>
     </header>
     </>
