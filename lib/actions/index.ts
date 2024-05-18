bea61eaa3b4e284d5a8ed3b559d71da8a7eeac1b
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-05-18 23:41:55 +0530

    this is how we create a instance or product in mongoDB

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,7 +1,9 @@
 'use server'
 
+import Product from "../models/product.model";
 import { connectToDB } from "../mongoose";
 import { scrapeAmazonProduct } from "../scraper";
+import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 
 export async function scrapeAndStoreProduct (productURL : string){
     if (!productURL) {
@@ -15,8 +17,34 @@ export async function scrapeAndStoreProduct (productURL : string){
 
         if(!scrapedProduct) return;
 
+        let product = scrapedProduct;
+
+        const existingProduct = await Product.findOne({ url : scrapedProduct.url});  
+
+        if(existingProduct){
+            const updatePriceHistory : any = [
+                ...existingProduct.priceHistory,
+                { price : scrapedProduct.currentPrice},
+            ]
+
+            product = {
+                ...scrapedProduct,
+                priceHistory : updatePriceHistory,
+                lowestPrice : getLowestPrice(updatePriceHistory),
+                highestPrice : getHighestPrice(updatePriceHistory),
+                averagePrice : getAveragePrice(updatePriceHistory),
+
+            }
+        }
+
+        const newProduct = await Product.findOneAndUpdate(
+            { url : scrapedProduct.url},
+            product,
+            { upsert : true , new : true}
+        )
+
         
     } catch (error : any) {
-        throw new Error (`Failed to Create/update Product : ${error.message}`)
+        throw new Error (`Failed to Create/upd ate Product : ${error.message}`)
     }
 }
