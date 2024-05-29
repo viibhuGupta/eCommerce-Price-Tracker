8a93d97f14fcebebdd11aecd34ffee389c1b3da7
Vibhu Gupta <vikram.kr.gupta01@gmail.com>
2024-05-29 15:53:52 +0530

    commit

--- components/PriceInfoCard.tsx
+++ components/PriceInfoCard.tsx
@@ -11,7 +11,7 @@ interface Props{
 const PriceInfoCard = ({title , iconSrc , value } : Props ) => {
   return (
     <div className={`price-info_card `}>
-    sr
+    
         <p className="text-base text-black-100 ">
             {title}
         </p>
